908650504d4ec366e3546385f4c732e6b285ffce
Ahmed <41826605+ElsinwarAhmed@users.noreply.github.com>
2024-05-22 14:13:57 +0300

    .

--- app/Http/Controllers/Api/V1/ConfigController.php
+++ app/Http/Controllers/Api/V1/ConfigController.php
@@ -314,7 +314,7 @@ class ConfigController extends Controller
         if ($validator->errors()->count()>0) {
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
-        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$request->lat.','.$request->lng.'&key=web-252@upbeat-palace-422604-g4.iam.gserviceaccount.com');
+        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$request->lat.','.$request->lng.'&key=AIzaSyCYxy0Q7Zqsf3jvUIdARmhtv0N_H29BtK4');
         return $response->json();
     }
 
